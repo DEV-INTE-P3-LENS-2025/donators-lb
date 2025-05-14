107caef31fa39ac1dc5e38b979d46172fa96f432
Sirolbfr <briquet.loris@gmail.com>
2025-05-14 15:23:38 +0200

    finished

--- script.js
+++ script.js
@@ -3,7 +3,6 @@
 const main = document.querySelector("main");
 let users = [];
 
-
 /*------ Fetch ------*/
 
 async function getData() {
@@ -17,78 +16,101 @@ async function getData() {
   return response.json();
 }
 
-getData().then(data => {
+getData().then((data) => {
   users = data.results;
+  users.forEach (user => {
+    let amt = (Math.floor(Math.random() * 39) * 10 + 10).toFixed(2); // Min : 10.00€; Max : 400.00€; Arondi à la dizaine; 2 chiffres après la virgule
+    user.amount = amt;
+  })
   display_cards(users);
-})
-
+});
 
 /*------ Main ------*/
 
-function gender_filter(obj, gender) {
+function gender_filter(tab, gender) {
   let filtered = [];
-  if (gender == "m") {
-    obj.forEach((user) => {
-      if (user.gender == "male") {
-        filtered.push(user);
-      }
-    });
-  } else {
-    obj.forEach((user) => {
-      if (user.gender == "female") {
-        filtered.push(user);
-      }
-    });
-  }
+  if (gender == "f") {gender = "female";}
+  else if (gender == "m") {gender = "male";}
+  tab.forEach (user => {
+    if (gender == "a" || gender == user.gender) {
+      filtered.push(user);
+    }
+  });
   return filtered;
 }
 
-function amount_sort(obj) {
-  let cache = [];
-  for (let i = 0; i < obj.length; i++) {
-  }
+function amount_sort(tab) {
+  return tab.sort((a,b) => b.amount - a.amount);
 }
 
-function abc_sort(obj){
+function abc_sort(tab) {
   let first_names = [];
-    for (let i = 0; i < obj.length; i++) {
-        first_names.push(obj[i].name.first);
+  for (let i = 0; i < tab.length; i++) {
+    first_names.push(tab[i].name.first);
+  }
+  first_names.sort();
+  let abc_sorted = [];
+  for (let j = 0; j < first_names.length; j++) {
+    for (let k = 0; k < tab.length; k++) {
+      if (first_names[j] == tab[k].name.first) {
+        abc_sorted.push(tab[k]);
+      }
     }
-    first_names.sort();
-    let abc_sorted = [];
-    for (let j = 0; j < first_names.length; j++) {
-        for (let k = 0; k < obj.length; k++) {
-            if (first_names[j] == obj[k].name.first) {
-                abc_sorted.push(obj[k]);
-            }
-        }
-    }
-    return abc_sorted;
+  }
+  return abc_sorted;
 }
 
 let current_filter = "";
 let current_sort = "";
-function filter_sort(filter, sort) {
+const f_op = document.querySelectorAll(".f_op");
+const s_op = document.querySelectorAll(".s_op");
+function filter_sort(option, filter, sort) {
+  // Add or remove class for CSS styling
+  const s_op_active = document.querySelectorAll(".s_op.active");
+  const f_op_active = document.querySelectorAll(".f_op.active");
+  if (filter != "") {
+    if (f_op_active.length != 0) {
+      f_op_active.forEach (elt => {
+        elt.classList.remove("active");
+      })
+    }
+    option.classList.add("active");
+  }
+  if (sort != "") {
+    if (s_op_active.length != 0) {
+      s_op_active.forEach (elt => {
+        elt.classList.remove("active");
+      })
+    }
+    option.classList.add("active");
+  }
+
   let res = [];
+
   // Filtering
-  if (filter == "") {filter = current_filter;}
-  if (filter == "a") {
-    res = users;
-    current_filter = "a";
+  if (filter == "") {
+    filter = current_filter;
+  }
+  if (filter == "f") {
+    res = gender_filter(users, "f");
   } else if (filter == "m") {
     res = gender_filter(users, "m");
-    current_filter = "m";
   } else {
-    res = gender_filter(users, "f");
-    current_filter = "f";
+    res = users;
   }
+  current_filter = filter;
+
   // Sorting
-  if (sort == "") {sort = current_sort;}
+  if (sort == "") {
+    sort = current_sort;
+  }
   if (sort == "abc") {
     res = abc_sort(res);
   } else if (sort == "amt") {
     res = amount_sort(res);
   }
+  current_sort = sort;
+
   display_cards(res);
 }
 
@@ -97,13 +119,12 @@ function create_card(user) {
   card.classList.add("card");
   const amnt = document.createElement("p");
   amnt.classList.add("amnt");
-  amnt.innerHTML = ((Math.floor(Math.random() * 39) * 10)+10).toFixed(2) + " €"; // Min : 10.00€; Max : 400.00€; Arondi à la dizaine; 2 chiffres après la virgule
+  amnt.innerHTML = user.amount + "€";
   card.appendChild(amnt);
   const img = document.createElement("img");
   img.classList.add("user_img");
   img.src = user.picture.large;
   img.alt = user.name.first + " " + user.name.last;
-  img.title = user.name.first + " " + user.name.last;
   card.appendChild(img);
   const name = document.createElement("p");
   name.classList.add("name");
@@ -131,16 +152,16 @@ function create_card(user) {
   phone_nbr.innerHTML = user.phone;
   phone.appendChild(phone_nbr);
   card.appendChild(phone);
-  
+
   return card;
 }
 
-function display_cards(obj) {
-  document.querySelectorAll(".card").forEach(elt => {
+function display_cards(tab) {
+  document.querySelectorAll(".card").forEach((elt) => {
     main.removeChild(elt);
   });
-  obj.forEach (user => {
+  tab.forEach((user) => {
     const card = create_card(user);
     main.appendChild(card);
-  })
+  });
 }
